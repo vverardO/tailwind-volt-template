9783b0f40faab1834a5929cad94cc3c79bd41c54
Valentim Verardo <vb.verardo@gmail.com>
2024-06-25 21:28:25 -0300

    feat: some fixes to routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+
+Route::fallback(function () {
+    return Redirect::route('welcome');
+});
 
 Volt::route('/acessar', 'login')->name('login');
 Volt::route('/registrar', 'register')->name('register');
-Volt::route('/bem-vindo', 'welcome')->name('welcome');
-Volt::route('/perfil', 'profile')->name('profile');
+
+Route::middleware(['auth'])->group(function () {
+    Volt::route('/bem-vindo', 'welcome')->name('welcome');
+    Volt::route('/perfil', 'profile')->name('profile');
+});
