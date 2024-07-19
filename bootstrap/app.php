8c7e2492d4dadf75d6792c54178e53403f8f24fc
Valentim Verardo <vb.verardo@gmail.com>
2024-07-18 23:12:06 -0300

    fix: fixing bootstrap app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
-    )->create();
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
